10ad9c268eedd12f800f89ac381100e8b9850e81
tempesttswinson <tempestt.swinson@gmail.com>
2020-05-22 11:00:49 -0400

    generate student ID of 5 digits with the first number representing their grade level

--- Student.java
+++ Student.java
@@ -1,28 +1,41 @@
 import java.util.Scanner;
 
 public class Student {
-    private int numOfStudents, studentID, tuitionBalance, gradeYear;
-    private int costOfCourse = 600;
-    private String firstName, lastName;
+    private int numOfStudents, tuitionBalance, gradeYear;
+    private String firstName, lastName, studentID;
     private String courses;// enums
+    // static variables mean that the property is true for all objects. Its not
+    // specific to just one object. It belongs to the class not just an instance of
+    // the object
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
     // constuctor prompts user to enter student's name and year
     public Student() {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter student first name: ");
+        System.out.print("Enter student first name: ");
         this.firstName = in.nextLine();
 
-        System.out.println("Enter student last name: ");
+        System.out.print("Enter student last name: ");
         this.lastName = in.nextLine();
 
         System.out.println("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level:");
         this.gradeYear = in.nextInt();
-        System.out.println(firstName + lastName + gradeYear);
+
+        setStudentID();
+
+        System.out.println(firstName + lastName + gradeYear + " " + studentID);
 
     }
 
-    // generate unique 5 digit ID for each student
+    // generate unique 5 digit ID for each student with the first number
+    // representing grade year
+    private void setStudentID() {
+        // Grade Year + ID
+        id++;
+        this.studentID = gradeYear + "" + id;
+    }
 
     // method for enrolling in courses
 
