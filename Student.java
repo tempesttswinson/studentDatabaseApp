2b961a59189c3aaa1ff76a49d6659a17f925075d
tempesttswinson <tempestt.swinson@gmail.com>
2020-05-22 15:34:53 -0400

    implement pay tuition method and fix error to git rid of null when printing courses. Create an array of objects. USer entered how many students to add then the new student was added to an array

--- Student.java
+++ Student.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Student {
-    private int numOfStudents, gradeYear;
+    private int gradeYear;
     private int tuitionBalance = 0;
     private String firstName, lastName, studentID;
-    private String courses = null;
+    private String courses = "";
     // static variables mean that the property is true for all objects. Its not
     // specific to just one object. It belongs to the class not just an instance of
     // the object
@@ -25,9 +25,6 @@ public class Student {
         this.gradeYear = in.nextInt();
 
         setStudentID();
-
-        System.out.println(firstName + lastName + gradeYear + " " + studentID);
-
     }
 
     // generate unique 5 digit ID for each student with the first number
@@ -42,26 +39,41 @@ public class Student {
     public void enroll() {
         // get inside a loop, user hits Q to exit
         do {
-            System.out.println("Enter course to enroll (Q to quit): ");
+            System.out.print("Enter course to enroll (Q to quit): ");
 
             Scanner in = new Scanner(System.in);
             String course = in.nextLine();
-            if (!course.equals("Q")) {
-                courses = courses + "\n" + course;
-                tuitionBalance = tuitionBalance + costOfCourse;
+            if (!course.equalsIgnoreCase("Q")) {
+                courses = courses + "\n " + course;
+                tuitionBalance += costOfCourse;
             } else {
                 break;
             }
         } while (1 != 0);
 
         System.out.println("ENROLLED IN: " + courses);
-        System.out.println("TUITION BALANCE: " + tuitionBalance);
     }
 
     // View balance
+    public void viewBalance() {
+        System.out.println("Your balance is: " + tuitionBalance);
+    }
 
     // method for paying tuition
+    public void payTuition() {
+        viewBalance();
+        System.out.print("Enter your payment: $");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        tuitionBalance -= payment;
+        System.out.println("Thank you for your payment of $" + payment);
+        viewBalance();
+    }
 
     // View Status
+    public String toString() {
+        return "Name: " + firstName + " " + lastName + "\nGrade Level: " + gradeYear + "\nStudent Id: " + studentID
+                + "\nCourses Enrolled: " + courses + "\nBalance: $" + tuitionBalance;
+    }
 
 }
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
-        Student stu1 = new Student();
-
-        stu1.enroll();
         // Ask how many new students the user wants to add
+        System.out.print("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+
+        // creates an array of Student objects
+        Student[] students = new Student[numOfStudents];
 
         // Create n number of students
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+
+        for (int n = 0; n < numOfStudents; n++) {
+            System.out.println(students[n].toString());
+        }
 
     }
 }
